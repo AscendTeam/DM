6e75f795a7bc9ba903c17cf375a03627ea4c10d9
WANG <amyamelia9527@gmail.com>
2019-12-31 08:34:55 +0800

    edit 二级路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,33 +9,49 @@ import Project from '../component/project/project'
 
 export default [
   {
-    path:'/home',//首页
-    component:Home
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',//首页
+    component: Home
+  },
+  {
+    path: '/whole',//全部
+    component: Whole,
+    children: [
+      {
+        path: "",
+        redirect: 'coffee'
+      },
+      {
+        path: 'coffee',
+        component: WholeCoffee
+      },
+      {
+        path: 'brand',
+        component: WholeBrand
+      }
+    ]
+  },
+  {
+    path: '/discover',//发现
+    component: Discover
+  },
+  {
+    path: '/wallet',//票夹
+    component: Wallet
 
   },
   {
-    path:'/whole',//全部
-    component:Whole,
-  },
-  {
-    path:'/discover',//发现
-    component:Discover
-
-  },
-  {
-    path:'/wallet',//票夹
-    component:Wallet
-
-  },
-  {
-    path:'/profile',//我的
-    component:Profile
+    path: '/profile',//我的
+    component: Profile
 
   },
 
   {
     // Project页面 临时放置
-    path:'/project',
-    component:Project
+    path: '/project',
+    component: Project
   },
 ]

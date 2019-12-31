00934e08981090b6ef76007aa938449d369a642b
WANG <amyamelia9527@gmail.com>
2019-12-31 15:24:29 +0800

    add server

--- src/pages/whole/whole.vue
+++ src/pages/whole/whole.vue
@@ -1,70 +1,47 @@
 <template>
   <div class="wholeComponent">
     <div class="C-router">
-      <div class="listRouter" :class="{active:isshow==1}" @click="isshow=1">
-          演出
-        
-      </div>
-      <div class="listRouter" :class="{active:isshow==2}" @click="isshow=2">
-        大咖
-      </div>
-      <div class="listRouter" :class="{active:isshow==3}" @click="isshow=3">
-        品牌
-      </div>
+        <router-link to="/whole/show" class="listRouter" :class="{active:$route.path==='/whole/show'}">演出</router-link>
+        <router-link to="/whole/coffee" class="listRouter" :class="{active:$route.path==='/whole/coffee'}">大咖</router-link>
+        <router-link to="/whole/brand" class="listRouter" :class="{active:$route.path==='/whole/brand'}">品牌</router-link>
     </div>
-
     <span class="C-search iconfont iconsearch"></span>
-    <Brand></Brand>
-    <Show v-if="isshow==1"/>
-    <Coffee v-if="isshow==2"/>
-    <Brand v-if="isshow==3"/>
     <div class="componentParcel">
-      <Coffee v-if="isshow==2"/>
-      <Brand v-if="isshow==3"/>
+        <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import Show from './show/show'
-import Coffee from './coffee/coffee'
-import Brand from './brand/brand'
-
   export default {
     components:{
-      Coffee,
-      Brand,
-       Show
     },
     data(){
       return{
-        isshow:2, //1.演出  2大咖  3品牌
-       
       }
     },
 
   }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus" type="text/stylus" scoped>
   .wholeComponent
     height 40px
     width 100%
+    // background-color #eee
     .C-router
-      
       display flex
-      // justify-content center
+      justify-content center
       .listRouter
-        
-        width 33.333%
         color #aaa
         padding 0 20px
         text-align center
-        font-size 14px
+        font-size 16px
         line-height 40px
-       
-            
-
+        &.active
+          color #000
+          font-size 20px
+          font-weight bold
     .C-search
       position absolute
       font-size 20px
